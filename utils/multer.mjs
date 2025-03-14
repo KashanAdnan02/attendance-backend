58eae0a892d92239aa7e6f7a47b1234331e87faa
Almiraadnan <almiraadnan34@gmail.com>
2025-03-14 21:05:43 +0500

    some-code

--- utils/multer.mjs
+++ utils/multer.mjs
@@ -1,14 +1,13 @@
-import multer from "multer";
+import multer from 'multer';
 
 const storage = multer.diskStorage({
-  destination: "./uploads/",
-  filename: function (req, file, cb) {
-    cb(null, `${new Date().getTime()}-${file.originalname}`);
+  destination: function (req, file, cb) {
+    cb(null, './uploads'); // Ensure this directory exists and is writable
   },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now());
+  }
 });
 
-const upload = multer({
-  storage: storage,
-});
-
+const upload = multer({ storage: storage });
 export { upload };
