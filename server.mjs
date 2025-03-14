577aa08a41b75db85da93b2f3f2c506fe42cb1a0
Almiraadnan <almiraadnan34@gmail.com>
2025-03-14 21:20:04 +0500

    some-code

--- server.mjs
+++ server.mjs
@@ -10,7 +10,7 @@ import cookieParser from "cookie-parser";
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://attendance-system-client-gilt.vercel.app/" }));
 app.use("/api/v1", student_route);
 app.use("/api/v1", attendance_route);
 
