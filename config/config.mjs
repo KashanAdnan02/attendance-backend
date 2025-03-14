631a61cbd5d9825494943e40f4408d3f21c1f130
Almiraadnan <almiraadnan34@gmail.com>
2025-03-14 18:03:26 +0500

    first commit

---
+++ config/config.mjs
@@ -0,0 +1,10 @@
+const PORT = 3000;
+const mongodb_uri =
+  "mongodb+srv://kashanadnan:ivYgqAbSHGg7xKkc@cluster0.ox00ado.mongodb.net/attendanceApplication?retryWrites=true&w=majority&appName=Cluster0";
+const JWTSecret = "KashanAdnanWithBrownieHackathonBestOfLuck";
+
+export default {
+  PORT,
+  mongodb_uri,
+  JWTSecret,
+};
